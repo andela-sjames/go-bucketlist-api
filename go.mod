e76115db9e4b3fdb03c7a5ede36d954617d0f424
andela-sjames <samuel.james@andela.com>
2019-08-02 16:22:48 +0100

    Add router for homehandler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andela-sjames/go-bucketlist-api
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.3
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// HomeHandler handles request to the base path and
+// return a simple welcome text
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":  "200",
+		"message": "Welcome to Bucketlist API",
+	})
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// Init Router
+	router := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	router.HandleFunc("/", HomeHandler)
+
+	// server block defined here
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8000",
+
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Starting server on port :8000")
+	log.Fatal(srv.ListenAndServe())
+
 }
