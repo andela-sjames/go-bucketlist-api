88bbee0080621d8dc47c36b131f2424a2e746dc0
andela-sjames <samuel.james@andela.com>
2019-08-02 16:59:58 +0100

    Use main.go in the meantime.

--- main.go
+++ main.go
@@ -9,6 +9,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// User field (Model) defined
+type User struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastNames string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+// Bucketlist field (Model) defined
+type Bucketlist struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Created  string `json:"date_created"`
+	Modified string `json:"date_modified"`
+	User     *User  `json:"user"`
+}
+
+// BucketlistItem field (Model) defined
+type BucketlistItem struct {
+	Name       string      `json:"name"`
+	Created    string      `json:"date_created"`
+	Modified   string      `json:"date_modified"`
+	Done       bool        `json:"done"`
+	Bucketlist *Bucketlist `json:"bucketlist"`
+}
+
 // HomeHandler handles request to the base path and
 // return a simple welcome text
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
