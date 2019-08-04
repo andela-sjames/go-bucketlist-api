dae04e6062035be97bd05c23a8119ae264dec7a0
andela-sjames <samuel.james@andela.com>
2019-08-04 15:06:31 +0100

    Update JWT auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,22 +1,35 @@
 package auth
 
-import "net/http"
+import (
+	"go-bucketlist-api/utils"
+	"net/http"
+)
 
 // JWTAuthenticationMiddleware defined to intercept request before passing
 // to the next middleware or handler
 func JWTAuthenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		notAuth := []string{"/api/user/new", "/api/user/login"} //List of endpoints that doesn't require auth
-		requestPath := r.URL.Path                               //current request path
+		noAuthRequiredURL := []string{"/api/user/new", "/api/user/login"} //List of endpoints that doesn't require auth
+		requestPath := r.URL.Path                                         //current request path
 
 		//check if request does not need authentication, serve the request if it doesn't need it
-		for _, value := range notAuth {
-
+		for _, value := range noAuthRequiredURL {
 			if value == requestPath {
 				next.ServeHTTP(w, r)
 				return
 			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
+
+		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
+			response = utils.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			utils.Respond(w, response)
+			return
 		}
 
 	})
