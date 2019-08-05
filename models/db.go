bcbbf341e966582bc9e99ee9627aa5e9209c0abd
andela-sjames <samuel.james@andela.com>
2019-08-05 17:16:05 +0100

    Update DB.go

--- models/db.go
+++ models/db.go
@@ -9,9 +9,7 @@ import (
 )
 
 const (
-	port   = 5432
-	user   = "postgres"
-	dbname = "postgres"
+	port = 5432
 )
 
 var db *gorm.DB
@@ -20,12 +18,10 @@ func init() {
 
 	password := os.Getenv("PGPASSWORD")
 	host := os.Getenv("PGHOST")
-	port := os.Getenv("PGPORT")
 	user := os.Getenv("PGUSER")
 	dbname := os.Getenv("PGDBNAME")
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	// db, err := sql.Open("postgres", psqlInfo)
 
 	conn, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
