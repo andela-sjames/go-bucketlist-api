b29f5879a21884784bafcaef8986aaed98e88885
andela-sjames <samuel.james@andela.com>
2019-08-06 21:34:04 +0100

    Update welcome file.

--- controllers/welcome.go
+++ controllers/welcome.go
@@ -8,10 +8,17 @@ import (
 // HomeHandler handles request to the base path and
 // return a simple welcome text
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{
-		"status":  "200",
-		"message": "Welcome to Bucketlist API",
-		"signup":  "/api/user/signup",
-		"login":   "/api/user/login",
+
+	type routes map[string]string
+	apiRoutes := make(routes)
+
+	apiRoutes["signup"] = "/api/user/signup"
+	apiRoutes["login"] = "/api/user/login"
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status":        "200",
+		"message":       "Welcome to Bucketlist API",
+		"documentation": "documentation_link_goes_here",
+		"api_routes":    apiRoutes,
 	})
 }
